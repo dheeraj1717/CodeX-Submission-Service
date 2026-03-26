a10372a00ad0b36fd18f432274fcec3932c55811
dheeraj1717 <dheerajjangid1717@gmail.com>
2026-03-26 22:59:56 +0530

    fix- service plugin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const fastify = require("fastify")({ logger: false });
 const app = require("./app");
+const connectDB = require("./config/dbConfig");
 
 const port = process.env.PORT || 4002;
 
 fastify.register(app);
 
-fastify.listen({ port }, (err) => {
+fastify.listen({ port }, async (err) => {
     if (err) {
         fastify.log.error(err);
         process.exit(1);
     }
+    // await connectDB();
     console.log(`Server listening on ${port}`);
 });
